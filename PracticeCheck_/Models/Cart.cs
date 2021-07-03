ecb1ff396e07a65493b2879b0bda37e159def3df
thilak15 <thilakreddy125@gmail.com>
2021-07-03 14:15:26 +0530

    Add project files.

---
+++ PracticeCheck_/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeCheck_.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        public int userId { get; set; }
+        public int menuItemId { get; set; }
+        public string menuItemName { get; set; }
+    }
+}
---
+++ PracticeCheck_/Models/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeCheck_.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Free Delivery")]
+        public bool freeDelivery { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime dateOfLaunch { get; set; }
+        public bool Active { get; set; }
+    }
+}
